466236167bdf041ac988c286cd6c4d48a17e85a0
Rashiq <rashiq.z@gmail.com>
2016-02-20 21:10:34 +0100

    Fix indentation

--- flask/server.py
+++ flask/server.py
@@ -39,15 +39,18 @@ def index():
 def linkedin_info():
     if 'linkedin_token' in session:
         me = linkedin.get('people/~:(id,first-name,last-name,formatted-name,headline,email-address,picture-url,picture-urls::(original),public-profile-url,location,industry,summary,specialties,positions:(id,title,summary,start-date,end-date,is-current,company:(id,name,type,size,industry,ticker)),educations:(id,school-name,field-of-study,start-date,end-date,degree,activities,notes),associations,interests,num-recommenders,date-of-birth,publications:(id,title,publisher:(name),authors:(id,name),date,url,summary),patents:(id,title,summary,number,status:(id,name),office:(name),inventors:(id,name),date,url),languages:(id,language:(name),proficiency:(level,name)),skills:(id,skill:(name)),certifications:(id,name,authority:(name),number,start-date,end-date),courses:(id,name,number),recommendations-received:(id,recommendation-type,recommendation-text,recommender),honors-awards,three-current-positions,three-past-positions,volunteer)')
+        
         print json.dumps(me.data)
-        return render_template('cv.html',
-        name=me.data['formattedName'],
-        tagline=me.data['headline'],
-        email=me.data['emailAddress'],
-        pictureUrl=me.data['pictureUrls']['values'][0],
-        linkedin=me.data['publicProfileUrl'],
-        jobs=me.data['positions']['values']
-    )
+
+        template = render_template('cv.html',
+            name=me.data['formattedName'],
+            tagline=me.data['headline'],
+            email=me.data['emailAddress'],
+            pictureUrl=me.data['pictureUrls']['values'][0],
+            linkedin=me.data['publicProfileUrl'],
+            jobs=me.data['positions']['values']
+        )
+        return template
 
     return redirect(url_for('login'))
 
